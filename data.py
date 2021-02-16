d32c64350ac083e05ae99448caec2bc3ac231b73
SpeTsaR <misyurailya5@gmail.com>
2021-02-16 10:03:54 +0200

    Code

---
+++ data.py
@@ -0,0 +1,15 @@
+username = ""
+password = ""
+
+direct_users_list = ['user1']
+
+users_settings_dict = {
+    'user1': {
+        'login': '',
+        'password': ''
+    },
+    # 'user2': {
+    #     'login': 'bot_login',
+    #     'password': 'bot_password'
+    # }
+}
